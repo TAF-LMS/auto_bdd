7cea4fc03dffe5d103b84aedcb8d0b38a7d56723
a.sechiho <a.sechiho@a1qa.com>
2020-04-04 20:10:56 +0300

    create chrome browser factory.

--- src/main/java/in/bntu/lms/framework/browser/Browsers.java
+++ src/main/java/in/bntu/lms/framework/browser/Browsers.java
@@ -1,12 +1,12 @@
 package in.bntu.lms.framework.browser;
 
+import in.bntu.lms.framework.browser.chrome.ChromeBrowserFactory;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Browsers {
-
     public static IBrowserFactory getFactory() {
-        return null;
+        return new ChromeBrowserFactory();
     }
 }
