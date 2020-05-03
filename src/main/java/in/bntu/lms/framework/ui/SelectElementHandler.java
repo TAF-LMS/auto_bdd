570a27294434390ed2960fda81aecd8cfb132eba
alekesey.sergey <a.sergey@a1qa.com>
2020-05-03 21:08:26 +0300

    update select

--- src/main/java/in/bntu/lms/framework/ui/SelectElementHandler.java
+++ src/main/java/in/bntu/lms/framework/ui/SelectElementHandler.java
@@ -1,6 +1,7 @@
 package in.bntu.lms.framework.ui;
 
 import in.bntu.lms.framework.ui.interfaces.Settable;
+import in.bntu.lms.util.ConditionWait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -14,13 +15,22 @@ public class SelectElementHandler extends BaseElement implements Settable<String
 
     @Override
     public void setValue(String value) {
-        click();
-        new Select(findElement()).selectByVisibleText(value);
-        click();
+        ConditionWait.waitForTrue(driver -> {
+            click();
+            Select select = new Select(findElement());
+            select.selectByVisibleText(value);
+            String value1 = findElement().getAttribute("value");
+            String selectedValue = select.getOptions()
+                    .stream()
+                    .filter(el -> el.getAttribute("value").equalsIgnoreCase(value1))
+                    .map(WebElement::getText)
+                    .findFirst()
+                    .orElse("");
+            return selectedValue.equalsIgnoreCase(value);
+        });
     }
 
     public List<WebElement> getOptions() {
         return new Select(findElement()).getOptions();
     }
-
 }
